c6dfbad6b05dbaaa09e009d73b3ea6ccf16503ee
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-11-22 11:58:26 +0100

    stringyfy

--- src/pages/api/papirdokument/opprett.ts
+++ src/pages/api/papirdokument/opprett.ts
@@ -14,8 +14,8 @@ const handler = async (
         const response = await opprettJournalpost(JSON.parse(req.body))
         let body = response.json()
 
-        console.log(`Response: ${response}`) // eslint-disable-line no-console
-        console.log(`Body: ${body}`) // eslint-disable-line no-console
+        console.log('Response: ' + JSON.stringify(response)) // eslint-disable-line no-console
+        console.log('Body: ' + JSON.stringify(body)) // eslint-disable-line no-console
 
         if (Object.keys(body).length === 0) {
             body = response.statusText

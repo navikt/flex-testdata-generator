65db3868a45aa118398dfa6945e5d3cc15227c36
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-01-24 09:22:09 +0100

    Bedre datovelging

--- src/components/friskmelding/FriskmeldingTilArbeidsformidling.tsx
+++ src/components/friskmelding/FriskmeldingTilArbeidsformidling.tsx
@@ -4,13 +4,18 @@ import {
     DatePicker,
     Select,
     TextField,
-    useRangeDatepicker,
+    useDatepicker,
 } from '@navikt/ds-react'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import ConfettiExplosion from 'react-confetti-explosion'
 
 import { FellesInputChildrenProps } from '../commoninput/CommonInput'
+import {
+    atStartOfWeek,
+    jsDateToLocalDate,
+    localDateToJsDate,
+} from '../../utils/localdate'
 
 const formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
 const localdateFormatter = DateTimeFormatter.ofPattern('yyyy-MM-dd')
@@ -19,13 +24,11 @@ export const FriskmeldingTilArbeidsformidling = (
     p: FellesInputChildrenProps
 ) => {
     const [begrunnelse, setBegrunnelse] = useState<string>(
-        'Han er frisk nok til å gjøre noe annet enn å jobbe med arbeid som innebærer smittefare.'
+        'Han er frisk nok til å gjøre noe annet enn å jobbe med arbeid som innebærer løfting av ting tyngre enn 20 kilo.'
     )
     const [status, setStatus] = useState<string>('FATTET')
     const [statusBy, setStatusBy] = useState<string>('Flex')
 
-    const [fom, setFom] = useState<LocalDate>(LocalDate.now())
-    const [tom, setTom] = useState<LocalDate>(LocalDate.now())
     const [isExploding, setIsExploding] = React.useState(false)
 
     const [statusAt, setStatusAt] = useState<string>(
@@ -34,31 +37,39 @@ export const FriskmeldingTilArbeidsformidling = (
 
     const [uuid, setUuid] = useState<string>(uuidv4())
     const [friskmelder, setFriskmelder] = useState<boolean>(false)
-    const { datepickerProps, toInputProps, fromInputProps } =
-        useRangeDatepicker({
-            defaultSelected: {
-                from: new Date(fom.toString()),
-                to: new Date(tom.toString()),
-            },
-            onRangeChange: (daterange) => {
-                if (daterange && daterange.from && daterange.to) {
-                    setFom(
-                        LocalDate.of(
-                            daterange.from.getFullYear(),
-                            daterange.from.getMonth() + 1,
-                            daterange.from.getDate()
-                        )
-                    )
-                    setTom(
-                        LocalDate.of(
-                            daterange.to.getFullYear(),
-                            daterange.to.getMonth() + 1,
-                            daterange.to.getDate()
-                        )
-                    )
-                }
-            },
-        })
+
+    const {
+        datepickerProps: fromDpProps,
+        inputProps: fromInputProps,
+        selectedDay: fra,
+        setSelected: setFra,
+    } = useDatepicker({
+        defaultSelected: localDateToJsDate(atStartOfWeek(LocalDate.now())),
+    })
+
+    const {
+        datepickerProps: tomDpProps,
+        inputProps: tomInputProps,
+        selectedDay: tom,
+        setSelected: setTom,
+    } = useDatepicker({
+        defaultSelected: localDateToJsDate(
+            atStartOfWeek(LocalDate.now()).plusWeeks(12).minusDays(1)
+        ),
+    })
+
+    const set12UkerFraDato = useCallback(
+        (dato: LocalDate) => {
+            setFra(localDateToJsDate(atStartOfWeek(dato)))
+            setTom(
+                localDateToJsDate(
+                    atStartOfWeek(dato).plusWeeks(12).minusDays(1)
+                )
+            )
+        },
+        [setFra, setTom]
+    )
+
     return (
         <div className="flex flex-col gap-y-6 mt-4">
             <TextField
@@ -96,7 +107,7 @@ export const FriskmeldingTilArbeidsformidling = (
             />
             <Select
                 label="Status"
-                className="w-32"
+                className="w-64"
                 value={status}
                 size="small"
                 onChange={(w) => setStatus(w.target.value)}
@@ -105,58 +116,118 @@ export const FriskmeldingTilArbeidsformidling = (
                 <option value="FERDIG_BEHANDLET">FERDIG_BEHANDLET</option>
             </Select>
 
-            <DatePicker {...datepickerProps}>
-                <div className="flex flex-wrap justify-center gap-4 my-4">
-                    <DatePicker.Input {...fromInputProps} label="Fra" />
-                    <DatePicker.Input {...toInputProps} label="Til" />
-                </div>
-            </DatePicker>
+            <div className="flex space-x-2">
+                <DatePicker {...fromDpProps}>
+                    <DatePicker.Input
+                        size="small"
+                        {...fromInputProps}
+                        label="Fra dato"
+                    />
+                </DatePicker>
+                <DatePicker {...tomDpProps}>
+                    <DatePicker.Input
+                        size="small"
+                        {...tomInputProps}
+                        label="Til dato"
+                    />
+                </DatePicker>
+            </div>
+
+            <div className="flex space-x-2">
+                <Button
+                    variant="secondary-neutral"
+                    size="small"
+                    onClick={() =>
+                        set12UkerFraDato(LocalDate.now().minusWeeks(12))
+                    }
+                >
+                    Fra 12 uker siden
+                </Button>
+                <Button
+                    variant="secondary-neutral"
+                    size="small"
+                    onClick={() =>
+                        set12UkerFraDato(LocalDate.now().minusWeeks(1))
+                    }
+                >
+                    Fra forrige uke
+                </Button>
+                <Button
+                    variant="secondary-neutral"
+                    size="small"
+                    onClick={() => set12UkerFraDato(LocalDate.now())}
+                >
+                    Fra denne uken
+                </Button>
+                <Button
+                    variant="secondary-neutral"
+                    size="small"
+                    onClick={() =>
+                        set12UkerFraDato(LocalDate.now().plusWeeks(1))
+                    }
+                >
+                    Fra neste uke
+                </Button>
+            </div>
 
             <Button
                 style={{ marginTop: '1em' }}
                 className="w-72"
                 loading={friskmelder}
                 onClick={async () => {
-                    if (p.fnr?.length != 11) {
-                        p.setError('Forventer 11 siffer')
-                        return
-                    }
-                    if (!begrunnelse) {
-                        p.setError('Trenger begrunnelse')
-                        return
-                    }
-                    const parsedStatusAt =
-                        OffsetDateTime.parse(statusAt).format(formatter)
-
-                    setFriskmelder(true)
-                    setIsExploding(false)
-                    const request: FriskmeldingVedtakKafkaMelding = {
-                        uuid: uuid,
-                        status: status,
-                        personident: p.fnr,
-                        begrunnelse: begrunnelse,
-                        fom: fom.format(localdateFormatter),
-                        tom: tom.format(localdateFormatter),
-                        statusAt: parsedStatusAt,
-                        statusBy: statusBy,
-                    }
-
-                    const res = await fetch(
-                        `/api/kafka/flex/test-isfrisktilarbeid-vedtak-status/${uuid}`,
-                        {
-                            method: 'POST',
-                            body: JSON.stringify(request),
+                    try {
+                        if (p.fnr?.length != 11) {
+                            p.setError('Forventer 11 siffer')
+                            return
                         }
-                    )
-                    const response = await res.text()
-                    if (res.ok) {
-                        p.setSuksess(`Vedtak ${uuid} opprettet`)
-                        setIsExploding(true)
-                    } else {
-                        p.setError(response)
+                        if (!begrunnelse) {
+                            p.setError('Begrunnelse må fylles ut')
+                            return
+                        }
+
+                        const parsedStatusAt =
+                            OffsetDateTime.parse(statusAt).format(formatter)
+                        if (fra!.getTime() >= tom!.getTime()) {
+                            p.setError('Fra dato må være før til dato')
+                            return
+                        }
+                        setFriskmelder(true)
+                        setIsExploding(false)
+                        const request: FriskmeldingVedtakKafkaMelding = {
+                            uuid: uuid,
+                            status: status,
+                            personident: p.fnr,
+                            begrunnelse: begrunnelse,
+                            fom: jsDateToLocalDate(fra!).format(
+                                localdateFormatter
+                            ),
+                            tom: jsDateToLocalDate(tom!).format(
+                                localdateFormatter
+                            ),
+                            statusAt: parsedStatusAt,
+                            statusBy: statusBy,
+                        }
+
+                        const res = await fetch(
+                            `/api/kafka/flex/test-isfrisktilarbeid-vedtak-status/${uuid}`,
+                            {
+                                method: 'POST',
+                                body: JSON.stringify(request),
+                            }
+                        )
+                        const response = await res.text()
+                        if (res.ok) {
+                            p.setSuksess(`Vedtak ${uuid} opprettet`)
+                            setIsExploding(true)
+                        } else {
+                            p.setError(response)
+                        }
+                        setFriskmelder(false)
+                        setUuid(uuidv4())
+                    } catch (e) {
+                        p.setError('Noe gikk galt: ' + JSON.stringify(e))
+                        setFriskmelder(false)
                     }
-                    setFriskmelder(false)
-                    setUuid(uuidv4())
                 }}
             >
                 Send vedtak om friskmelding

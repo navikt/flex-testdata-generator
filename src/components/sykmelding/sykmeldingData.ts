958558c1cca9827522caa5b563c54b753d687f49
Eirik HS <eirik.skjaerseth@gmail.com>
2025-02-27 14:43:16 +0100

    format

--- src/components/sykmelding/sykmeldingData.ts
+++ src/components/sykmelding/sykmeldingData.ts
@@ -40,7 +40,7 @@ export function genererSykmeldingMedBehandlingsutfallKafkaMelding(
             rules: [],
         },
         metadata: {
-            type: "EGENMELDT",
+            type: 'EGENMELDT',
             msgInfo: {
                 type: 'SYKMELDING',
                 genDate: OffsetDateTime.parse('2021-01-01T00:00:00.00Z'),

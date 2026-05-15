f3f1aea9f6c2bd90635bc0e59f6d8d7770ad700c
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2026-05-15 07:37:08 +0200

    Oppgrader til Aksel 8

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,11 @@ module.exports = {
         extend: {},
     },
     plugins: [],
+    corePlugins: {
+        // Aksel v8 inkluderer sin egen reset i @layer aksel.reset. Tailwinds preflight
+        // genererer unlayered CSS (border-width: 0, button background-color osv.) som
+        // overstyrer alle @layer uavhengig av specificity, og dermed fjerner
+        // Aksel-komponentenes sine styles.
+        preflight: false,
+    },
 }

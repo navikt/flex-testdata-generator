b74933c61714ed71b821a918812bee496d212bfc
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2026-05-15 09:05:15 +0200

    Bruk Aksel Internal Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,52 @@
 import '../style/global.css'
 
-import { Heading, Link } from '@navikt/ds-react'
+import { Dropdown, InternalHeader, Spacer } from '@navikt/ds-react'
+import { MenuHamburgerIcon } from '@navikt/aksel-icons'
 import type { AppProps } from 'next/app'
 import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <>
-            <Heading
-                className="flex py-4 pl-72 border-b-2 border-black mb-4"
-                size="xlarge"
-            >
-                Flex Testdata
-            </Heading>
-            <div className="flex">
-                <ul className="w-72 pl-4">
-                    <li>
-                        <Link href="/">Reset testdata</Link>
-                    </li>
-                    <li>
-                        <Link href="/ditt-sykefravaer-melding">
-                            Send melding til Ditt Sykefravaer
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/papir-dokument">Papirdokument</Link>
-                    </li>
-                    <li>
-                        <Link href="/friskmelding-til-arbeidsformidling">
-                            Friskmeld til arbeidsformidling
-                        </Link>
-                    </li>
-                </ul>
-                <div className="flex-auto">
-                    <Component {...pageProps} />
-                </div>
-            </div>
+            <InternalHeader>
+                <InternalHeader.Title href="/">
+                    Flex Testdata
+                </InternalHeader.Title>
+                <Spacer />
+                <Dropdown>
+                    <InternalHeader.Button as={Dropdown.Toggle}>
+                        <MenuHamburgerIcon title="Meny" />
+                    </InternalHeader.Button>
+                    <Dropdown.Menu>
+                        <Dropdown.Menu.List>
+                            <Dropdown.Menu.List.Item as="a" href="/">
+                                Reset testdata
+                            </Dropdown.Menu.List.Item>
+                            <Dropdown.Menu.List.Item
+                                as="a"
+                                href="/ditt-sykefravaer-melding"
+                            >
+                                Send melding til Ditt Sykefravaer
+                            </Dropdown.Menu.List.Item>
+                            <Dropdown.Menu.List.Item
+                                as="a"
+                                href="/papir-dokument"
+                            >
+                                Papirdokument
+                            </Dropdown.Menu.List.Item>
+                            <Dropdown.Menu.List.Item
+                                as="a"
+                                href="/friskmelding-til-arbeidsformidling"
+                            >
+                                Friskmeld til arbeidsformidling
+                            </Dropdown.Menu.List.Item>
+                        </Dropdown.Menu.List>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </InternalHeader>
+            <main className="mt-8">
+                <Component {...pageProps} />
+            </main>
         </>
     )
 }

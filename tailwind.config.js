7364a1ea32e6cb228468c741244e0c8e46615380
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2026-05-19 21:28:33 +0200

    Oppgrader til Tailwind 4 (#1557)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 const naviktTailwindPreset = require('@navikt/ds-tailwind')
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     presets: [naviktTailwindPreset],
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {},
-    },
-    plugins: [],
-    corePlugins: {
-        // Aksel v8 inkluderer sin egen reset i @layer aksel.reset. Tailwinds preflight
-        // genererer unlayered CSS (border-width: 0, button background-color osv.) som
-        // overstyrer alle @layer uavhengig av specificity, og dermed fjerner
-        // Aksel-komponentenes sine styles.
-        preflight: false,
     },
 }
